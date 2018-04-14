9cb099e84240fe2ee773a18ab0294d53b9f3b29d
Casper Strömberg <casper@barkh.se>
2018-04-14 16:28:41 +0100

    new autocomplete

--- src/AHAutocomplete/AHAutocomplete.js
+++ src/AHAutocomplete/AHAutocomplete.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import * as React from "react";
 import Autosuggest from "react-autosuggest-ie11-compatible";
 import theme from "./theme.css";
 import { AHDetailButton } from "../AHDetailButton/AHDetailButton";
@@ -14,35 +14,75 @@ const wrapperOfTheWrapperStyle = {
   marginRight: "5%"
 };
 
-export const AHAutocomplete = withTheme(props => {
-  const { style, loading, inputProps, autoRef, ...childProps } = props;
-  const { onChange, ...restInputProps } = inputProps;
+type Item = {};
 
-  return (
-    <div style={{...wrapperOfTheWrapperStyle, ...style}}>
-      {props.clearButton &&
-        <AHDetailButton
-          onClick={props.clearButtonOnClick}
-          style={{ border:"0" }}
-        >
-          ✕
-        </AHDetailButton>}
-      {loading &&
-        <i
-          style={{ position: "absolute", right: 20, zIndex: 4 }}
-          className="fa fa-spinner fa-spin fa-1x fa-fw"
-        />}
-      <Autosuggest
-        ref={autoRef}
-        {...childProps}
-        inputProps={{
-          ...restInputProps,
-          onChange: (e, val) => {
-            onChange(e, val ? val : { newValue: e.target.value });
-          }
-        }}
-        theme={theme}
-      />
-    </div>
-  );
-});
+type Props = {
+  items: Item[],
+  onChange: (newValue: string) => void,
+  value: string,
+  getItemValue: Item => string,
+
+  clearButton?: boolean,
+  clearButtonOnClick?: Event => void,
+  style?: {},
+  loading?: boolean,
+  inputProps?: {},
+  autoRef?: any
+};
+
+export class AHAutocomplete extends React.Component<Props, any> {
+  state = {
+    items: []
+  };
+
+  onSuggestionsFetchRequested = () => {
+    this.setState({ items: this.props.items });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({ items: [] });
+  };
+
+  render() {
+    const {
+      value,
+      items,
+      onChange,
+      clearButton,
+      clearButtonOnClick,
+      style,
+      loading,
+      inputProps,
+      autoRef,
+      ...childProps
+    } = this.props;
+
+    return (
+      <div style={{ ...wrapperOfTheWrapperStyle, ...style }}>
+        {clearButton && (
+          <AHDetailButton onClick={clearButtonOnClick} style={{ border: "0" }}>
+            ✕
+          </AHDetailButton>
+        )}
+        {loading && (
+          <i
+            style={{ position: "absolute", right: 20, zIndex: 4 }}
+            className="fa fa-spinner fa-spin fa-1x fa-fw"
+          />
+        )}
+        <Autosuggest
+          ref={autoRef}
+          {...childProps}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          inputProps={{
+            value: value,
+            onChange: (e, val) => {
+              onChange(val && val.newValue ? val.newValue : e.target.value);
+            }
+          }}
+        />
+      </div>
+    );
+  }
+}
--- src/AHAutocomplete/AHAutocomplete.md
+++ src/AHAutocomplete/AHAutocomplete.md
@@ -1,15 +1,35 @@
 ```js
+suggestions = [
+  {
+    name: "C",
+    year: 1972
+  },
+  {
+    name: "Elm",
+    year: 2012
+  }
+];
+
+initialState = {
+  value: "",
+  suggestions: []
+};
+
 <AHAutocomplete
-  inputProps={{ onChange: function() {} }}
-  suggestions={[
-    {
-      name: "C",
-      year: 1972
-    },
-    {
-      name: "Elm",
-      year: 2012
-    }
-  ]}
-/>
+  onSuggestionsFetchRequested={() => setState({ suggestions: suggestions })}
+  onSuggestionsClearRequested={() => setState({ suggestions: [] })}
+  inputProps={{
+    onChange: (_, { newValue }) => setState({ value: newValue }),
+    value: state.value
+  }}
+  getSuggestionValue={s => s.name}
+  renderSuggestion={function(s) {
+    return (
+      <div>
+        {s.name} <div>{s.year}</div>
+      </div>
+    );
+  }}
+  suggestions={state.suggestions}
+/>;
 ```
